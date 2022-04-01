07dd8df1c103014a45b1987a1e313f6a7b850eb9
Mauricio Amorim <mjvamorim@gmail.com>
2022-04-01 14:23:08 -0300

    Client Router Instalado

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,6 @@
-import { useState } from 'react';
 import './App.css';
-import { Tweet } from './components/Tweet';
+import { AppRoutes } from './AppRoutes';
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      Qtde Cliques {count}
-      <Tweet text="Tweet 01" />
-      <Tweet text="Tweet 02" />
-      <Tweet text="Tweet 03" />
-      <button type="button" onClick={() => setCount(count + 1)}>
-        Adicionar tweet
-      </button>
-    </div>
-  );
+  return <AppRoutes />;
 }
